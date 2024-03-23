a0e03cb7e83c3e0f76fb051c6b7a0290c0ffc813
yukaymmt <yamamtyuka@gmail.com>
2024-03-23 09:50:29 -0400

    delete all the unnessecary comments

--- serial.c
+++ serial.c
@@ -25,8 +25,6 @@ void Zem_init(Zem_t *s, int value){
 	s->value = value;
 	int rc = pthread_cond_init(&s->cond, NULL);
 	assert (rc == 0);
-	// rc = pthread_mutex_init(&s->lock, NULL);
-	// assert (rc == 0);
 }
 
 void Zem_wait(Zem_t *s){
@@ -34,13 +32,6 @@ void Zem_wait(Zem_t *s){
 	while(s->value <= 0){pthread_cond_wait(&s->cond, &s->lock);}
 	s->value--;
 	pthread_mutex_unlock(&s->lock);
-	// if (pthread_mutex_trylock(&s->lock) == 0){
-	// 	while(s->value <= 0){pthread_cond_wait(&s->cond, &s->lock);}
-	// 	s->value--;
-	// 	pthread_mutex_unlock(&s->lock);}
-	// else{
-	// 	EBUSY;
-	// }
 }
 
 void Zem_post(Zem_t *s){
@@ -48,13 +39,6 @@ void Zem_post(Zem_t *s){
 	s->value++;
 	pthread_cond_signal(&s->cond);
 	pthread_mutex_unlock(&s->lock);
-	// if (pthread_mutex_trylock(&s->lock) == 0){
-	// 	s->value++;
-	// 	pthread_cond_signal(&s->cond);
-	// 	pthread_mutex_unlock(&s->lock);}
-	// else{
-	// 	EBUSY;
-	// }
 }
 
 //Read-Write Lock using Zemaphore
@@ -126,8 +110,6 @@ pthread_cond_t cond_p = PTHREAD_COND_INITIALIZER;
 int total_in = 0, total_out = 0;
 rwlock_t rw_total_in;
 rwlock_t rw_total_out;
-//lock for malloc()
-//rwlock_t rw_malloc;
 //lock for file
 rwlock_t rw_file;
 //lock for args
@@ -151,19 +133,15 @@ void *thread_createSingleZippedPackage(void* arg){
 
 	//allocate heap for memory
 	//allocating heap is safe in C so lock might be unnesasarry in terms of speed (not sure)
-	//rwlock_acquire_writelock(&rw_malloc);
 	char *full_path = malloc(len*sizeof(char));
-	//rwlock_release_writelock(&rw_malloc);
 	
 	assert(full_path != NULL);
 	strcpy(full_path, argv[1]);
 	strcat(full_path, "/");
 	strcat(full_path, files[i]);
 
-	// rwlock_acquire_writelock(&rw_malloc);
 	unsigned char *buffer_in = malloc(BUFFER_SIZE * sizeof(unsigned char));
 	unsigned char *buffer_out = malloc(BUFFER_SIZE * sizeof(unsigned char));
-	// rwlock_release_writelock(&rw_malloc);
 
 	// load file
 	rwlock_acquire_readlock(&rw_file);
@@ -196,14 +174,7 @@ void *thread_createSingleZippedPackage(void* arg){
 	total_out += nbytes_zipped;
 	rwlock_release_writelock(&rw_total_out);
 
-	// rwlock_acquire_writelock(&rw_malloc);
 	free(full_path);
-	// rwlock_release_writelock(&rw_malloc);
-
-	//Execute fwrite() based on priority
-	//unlock before waiting and skeep for 1ms to reduce CPU usage
-	//lock again before re-checking
-	// pthread_mutex_lock(&mutex_p);
 
 	pthread_mutex_lock(&mutex_p);
 	while(priority != next_priority){
@@ -217,35 +188,11 @@ void *thread_createSingleZippedPackage(void* arg){
 	pthread_cond_broadcast(&cond_p);
 	pthread_mutex_unlock(&mutex_p);
 
-	// if (pthread_mutex_trylock(&mutex_p) == 0){
-	// 	while(priority != next_priority){
-	// 		pthread_cond_wait(&cond_p, &mutex_p); 
-	// 	}
-	// 	rwlock_acquire_writelock(&rw_file);
-	// 	fwrite(&nbytes_zipped, sizeof(int), 1, f_out);
-	// 	fwrite(buffer_out, sizeof(unsigned char), nbytes_zipped, f_out);
-	// 	rwlock_release_writelock(&rw_file);
-	// 	next_priority++;
-	// 	pthread_cond_broadcast(&cond_p);
-	// 	pthread_mutex_unlock(&mutex_p);}
-	// else{
-	// 	EBUSY;
-	// }
-
-
-
 	//Reduce the active_thread number and signal for the cond
 	pthread_mutex_lock(&mutex);
 	num_active_threads--;
 	pthread_mutex_unlock(&mutex);
-	
-	// if (pthread_mutex_trylock(&mutex) == 0){
-	// 	num_active_threads--;
-	// 	pthread_mutex_unlock(&mutex);}
-	// else{
-	// 	EBUSY;
-	// }
-	
+
 	pthread_cond_signal(&cond);
 
 	pthread_exit(NULL);
@@ -299,7 +246,6 @@ int main(int argc, char **argv) {
 	//rw_lock
 	rwlock_init(&rw_total_in); //for total_in
 	rwlock_init(&rw_total_out); //for total_out
-	//rwlock_init(&rw_malloc); //for memory access (for free() and malloc())
 	rwlock_init(&rw_file); //for f_out
 	rwlock_init(&rw_args); //for args
 
@@ -313,20 +259,6 @@ int main(int argc, char **argv) {
 		args.f_out = f_out;
 		args.priority = i;
 		rwlock_release_writelock(&rw_args);
-
-		//cannot run more than 20 threads at the same time
-		// pthread_mutex_lock(&mutex);
-		// while (num_active_threads >= MAX_THREADS){
-		// 	pthread_cond_wait(&cond, &mutex);
-		// }
-		// //if num_active_threads is lower than 20, create thread
-		// rwlock_acquire_readlock(&rw_args);
-		// if (pthread_create(&threads[i], NULL, thread_createSingleZippedPackage, (void*)&args) != 0){
-		// 	exit(EXIT_FAILURE);
-		// };
-		// num_active_threads++;
-		// pthread_mutex_unlock(&mutex);
-
 
 		if (pthread_mutex_trylock(&mutex) == 0){
 			while (num_active_threads >= MAX_THREADS){
